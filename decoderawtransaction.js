4c19e072be9378601fbbbd829e8b52562c7bc1ec
k-matsuzawa <matsuzawa@cryptogarage.co.jp>
2020-06-28 18:00:41 +0900

    test

---
+++ decoderawtransaction.js
@@ -0,0 +1,32 @@
+const updateField = async function(event) {
+  const inputTx = document.getElementById("inputTx");
+  const decodedtx = document.getElementById("decodedtx");
+  try {
+    const req = {
+      hex: inputTx.value,
+      network: 'liquidregtest',
+      mainchainNetwork: 'regtest',
+    };
+    const resp = await callJsonApi('ElementsDecodeRawTransaction', req);
+    decodedtx.value = resp.hex;
+    return;
+  } catch (e) {
+  }
+
+  try {
+    const req = {
+      hex: inputTx.value,
+      network: 'regtest',
+    };
+    const resp = await callJsonApi('DecodeRawTransaction', req);
+    decodedtx.value = resp.hex;
+    return;
+  } catch (e) {
+    decodedtx.value = 'Invalid transaction format';
+  }
+}
+
+Module['onRuntimeInitialized'] = async function(){
+  // set decoderawtx function on field change
+  document.body.addEventListener("change", updateField);
+}
